7ad635045eae2f6b4f47ccae3a2034e99faa744c
AbduallahAtta <33333259+AbduallahAtta@users.noreply.github.com>
2018-08-24 09:55:49 +0200

    enhanced RegisterActivity UI

--- app/src/main/java/elamien/abdullah/shopemall/activities/RegisterActivity.java
+++ app/src/main/java/elamien/abdullah/shopemall/activities/RegisterActivity.java
@@ -1,7 +1,11 @@
 package elamien.abdullah.shopemall.activities;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.text.SpannableString;
 import android.text.Spanned;
@@ -12,22 +16,42 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import elamien.abdullah.shopemall.GlideApp;
 import elamien.abdullah.shopemall.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
     @BindView(R.id.loginMemberLabelTextView)
     TextView existedMembershipLabel;
+    @BindView(R.id.registerLayoutParent)
+    ConstraintLayout registerLayoutParent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         ButterKnife.bind(this);
+      //  loadBackgroundImage();
         setupExistedMembershipLabel();
+    }
+
+    private void loadBackgroundImage() {
+        GlideApp.with(this)
+                .load("https://goo.gl/qExfGq")
+                .placeholder(R.drawable.place_holder)
+                .error(R.drawable.error_holder)
+                .into(new SimpleTarget<Drawable>() {
+                    @Override
+                    public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                        registerLayoutParent.setBackground(resource);
+                    }
+                });
     }
 
     private void setupExistedMembershipLabel() {
